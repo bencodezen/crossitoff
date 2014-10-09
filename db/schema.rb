f2d00cd6337cf8c0dbcc52fabbd026fc7a5d2321
bengozen <bengozen@gmail.com>
2014-10-08 20:24:07 -0400

    Added Items model and added associations with user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140926133648) do
+ActiveRecord::Schema.define(version: 20141009001552) do
+
+  create_table "items", force: true do |t|
+    t.string   "title"
+    t.boolean  "complete"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
