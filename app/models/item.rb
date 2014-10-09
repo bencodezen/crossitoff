044a7119e98cdff23843333a4a4aa9302075bee6
bengozen <bengozen@gmail.com>
2014-10-09 15:53:20 -0400

    Added time remaining calculation to Items.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+
+  def remaining_time
+    new_date = self.created_at + 7.days
+    time_remaining = (new_date - self.created_at) / 24.hour.seconds
+    time_remaining.to_i
+  end
 end
