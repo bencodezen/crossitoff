580ac6ff2f516f0d95c12c5e98661cdd90572af4
bengozen <bengozen@gmail.com>
2014-11-06 09:26:03 -0500

    Fixed email sending functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 CrossItOff::Application.routes.draw do
   devise_for :users
 
-  resources :users do
-    resources :items, only: [:index, :create, :destroy], controller: 'users/items'
+  namespace :users do
+    resources :items, only: [:index, :create, :destroy]
   end
   
   get 'about' => "welcome#about"
